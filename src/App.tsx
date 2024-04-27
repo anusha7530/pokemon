d9b65ab12c83714b3a62c795e8850c8c4c0cce92
anusha7530 <agarwalanusha13@gmail.com>
2024-04-28 02:20:32 +0530

    Created sass file and update background

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import Navbar from './sections/Navbar'
 import Wrapper from './sections/Wrapper'
 import Footer from './sections/Footer'
+import Background from './components/Background'
+import "./scss/index.scss";
 
 function App() {
   return (
     <div className='main-container'>
+      <Background />
       <div className="app">
         <Navbar />
         <Wrapper />
