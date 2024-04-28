2c6c9872eb7612d4aa3d452e1ca3342d605e23e8
anusha7530 <agarwalanusha13@gmail.com>
2024-04-28 20:54:51 +0530

    Added pokeApi and create reducers

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Wrapper from '../sections/Wrapper'
+import { useAppDispatch, useAppSelector } from '../app/hooks'
+import { getInitialPokemonData } from '../app/reducers/getInitialPokemonData';
 
 function Search() {
+  const dispatch = useAppDispatch();
+  const {allPokemon} = useAppSelector(({pokemon}) =>pokemon);
+  useEffect(()=>{
+    dispatch(getInitialPokemonData());
+  },[dispatch]);
+
+  useEffect(() =>{
+    if(allPokemon){
+      const clonedPokemons = [...allPokemon];
+      const randomPokemonsId = clonedPokemons.sort(() => Math.random()-Math.random()).slice(0,20);
+    }
+  },[allPokemon]);
+
   return (
     <div>
       search
@@ -9,4 +24,4 @@ function Search() {
   )
 }
 
-export default Wrapper(Search)
+export default Wrapper(Search);
